1ed0c0a97f9372d86da9a802fa3fc766218843e5
Rahul Yadav <rahul.yadav6131766@gmail.com>
2023-12-24 22:02:02 +0530

    Added refresh access token

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
@@ -153,4 +154,63 @@ export const userLogout = asyncHandler(async (req, res) => {
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+// Generating Refresh Access Token
+export const refreshAccessToken = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Getting refresh token from cookie
+   * TODO: Decode refresh token data
+   * TODO: Check if user exists with that token
+   * TODO: Comparing cookie refresh token with user refresh token stored in DB
+   * TODO: If True, Generating new access token
+   * TODO: Sending response with new access token in cookie
+   * **/
+
+  //* Gettig refresh token
+  const incomingRefreshToken =
+    req.cookies?.refreshToken || req.body?.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request!");
+  }
+
+  try {
+    //* Decoding refresh token
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    //* Getting user by refresh token
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token!");
+    }
+
+    //* Comparing cookie refresh token with user refresh token
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refres token is expired!");
+    }
+
+    //* Generating new Access token
+    const { accessToken, refreshToken } = await generateAccessAndRefresToken(
+      user._id
+    );
+
+    //* Response
+    res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken },
+          "Access token refreshed!"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token!");
+  }
 });
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  refreshAccessToken,
   userLogin,
   userLogout,
   userRegistration,
@@ -12,5 +13,6 @@ const userRouter = Router();
 userRouter.post("/register", upload.single("avatar"), userRegistration);
 userRouter.post("/login", userLogin);
 userRouter.post("/logout", verifyJWT, userLogout);
+userRouter.post("/refresh-access", refreshAccessToken);
 
 export default userRouter;
