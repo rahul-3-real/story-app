499f20a1382b34874348fb6cd5094404cd67bc51
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-10-27 00:02:53 +0530

    Backend - Added Logout Api

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,12 +3,15 @@ import { Router } from "express";
 import {
   loginController,
   registerController,
+  logoutController,
 } from "../controllers/user.controllers.js";
+import { verifyUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // Routes
 router.route("/register").post(registerController);
 router.route("/login").post(loginController);
+router.route("/logout").post(verifyUser, logoutController);
 
 export default router;
