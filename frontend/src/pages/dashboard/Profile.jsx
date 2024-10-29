f435958a333ce1afc864720b7b7016609e0f90e7
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-10-30 02:48:40 +0530

    Frontend - Added Base Profile UI

--- frontend/src/pages/dashboard/Profile.jsx
+++ frontend/src/pages/dashboard/Profile.jsx
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { MdVerified } from "react-icons/md";
+import { MdOutlineModeEdit } from "react-icons/md";
+
 import { Breadcrumb } from "../../components/dashboard";
+import { DummyProfile } from "../../static/images";
 
 const Profile = () => {
   const breadcrumbData = {
@@ -6,10 +12,59 @@ const Profile = () => {
     Profile: "current",
   };
 
+  const user = useSelector((state) => state.auth.user);
+
+  const imageUrl = user?.avatar
+    ? `${import.meta.env.VITE_API_DOMAIN}/static/${user.avatar.replace(
+        /public_draft[\\/]/,
+        ""
+      )}`
+    : DummyProfile;
+
   return (
     <div className="container">
       <Breadcrumb data={breadcrumbData} />
-      <h1>Profile</h1>
+
+      <div className="profile-content">
+        <div className="profile-image">
+          <img src={imageUrl} alt={user.full_name} />
+        </div>
+        <div className="profile-info">
+          <div className="profile-actions">
+            <button className="button button-outline button-sm">Message</button>
+            <button className="button button-sm">Follow</button>
+          </div>
+
+          <h2>
+            {user.full_name} {!user.verified && <MdVerified title="Verified" />}
+          </h2>
+
+          <div className="follow-list">
+            <Link to="/profile" className="follow-link">
+              <span>30</span>
+              Posts
+            </Link>
+            <Link to="/profile" className="follow-link">
+              <span>1.5K</span>
+              Followers
+            </Link>
+            <Link to="/profile" className="follow-link">
+              <span>115</span>
+              Following
+            </Link>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            praesentium eius repellat at quis ipsum?
+          </p>
+        </div>
+      </div>
+
+      <Link to="/profile/edit">
+        <MdOutlineModeEdit />
+        <span>Edit Profile</span>
+      </Link>
     </div>
   );
 };
