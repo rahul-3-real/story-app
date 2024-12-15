77cf5c293e93b304530b73864fb4cf6d1cfba8d2
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-12-15 17:25:01 +0530

    Backend - Added Story CRUD API

--- backend/src/controllers/story.controllers.js
+++ backend/src/controllers/story.controllers.js
@@ -65,3 +65,90 @@ export const createStoryController = asyncHandler(async (req, res) => {
       )
     );
 });
+
+// Get Story Detail Controller
+export const storyDetailController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Get Story id from request
+   * TODO: Search Story
+   * TODO: Sending Response
+   * **/
+
+  // * Get Story id from request
+  const { _id } = req.query;
+
+  // * Search Story
+  const story = await Story.findById(_id);
+  if (!story) throw new ApiError(404, "Story not found");
+
+  // * Sending Response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { story }, "Story fetched successfully!"));
+});
+
+// Update Story Controller
+export const updateStoryController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Get data from frontend
+   * TODO: Check if story exists
+   * TODO: Validate Story
+   * TODO: Update Story
+   * TODO: Sending Response
+   * **/
+
+  // * Get data from frontend
+  const { title, description, genre } = req.body;
+  const { _id } = req.query;
+  const poster = req.file?.path;
+
+  // * Check if story exists
+  const story = await Story.findById(_id);
+  if (!story) throw new ApiError(404, "Story not found");
+
+  // * Validate Story
+  if (title) {
+    minLengthValidation(title, 3, "Title");
+
+    // Check for existing story with the same title
+    const storyExists = await Story.findOne({ title, _id: { $ne: _id } });
+    if (storyExists)
+      throw new ApiError(400, "Story with this title already exists");
+  }
+  if (description) minLengthValidation(description, 50, "Description");
+  if (genre) notEmptyArrayValidation(genre, "Genre");
+
+  // * Update Story
+  story.title = title || story.title;
+  story.description = description || story.description;
+  story.genre = genre || story.genre;
+  story.poster = poster || story.poster;
+  await story.save();
+
+  // * Sending Response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { story }, "Story updated successfully!"));
+});
+
+// Delete Story Controller
+export const deleteStoryController = asyncHandler(async (req, res) => {
+  /**
+   * TODO: Get Story id from request
+   * TODO: Check if story exists
+   * TODO: Delete Story
+   * TODO: Sending Response
+   * **/
+
+  // * Get Story id from request
+  const { _id } = req.query;
+
+  // * Check if story exists
+  const story = await Story.findByIdAndDelete(_id);
+  if (!story) throw new ApiError(404, "Story not found");
+
+  // * Sending Response
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Story deleted successfully!"));
+});
--- backend/src/routes/story.routes.js
+++ backend/src/routes/story.routes.js
@@ -2,9 +2,16 @@ import { Router } from "express";
 
 import {
   createStoryController,
+  deleteStoryController,
   fetchStoriesController,
+  storyDetailController,
+  updateStoryController,
 } from "../controllers/story.controllers.js";
-import { isAuthor, verifyUser } from "../middlewares/auth.middleware.js";
+import {
+  isAuthor,
+  isAuthorized,
+  verifyUser,
+} from "../middlewares/auth.middleware.js";
 import uploadMiddleware from "../middlewares/multer.middleware.js";
 
 const router = new Router();
@@ -18,6 +25,24 @@ router
     isAuthor,
     uploadMiddleware("poster", ["jpg", "jpeg", "png", "gif", "svg"]),
     createStoryController
+  );
+router.route("/").get(storyDetailController);
+router
+  .route("/")
+  .patch(
+    verifyUser,
+    isAuthor,
+    isAuthorized("Story", "author"),
+    uploadMiddleware("poster", ["jpg", "jpeg", "png", "gif", "svg"]),
+    updateStoryController
+  );
+router
+  .route("/")
+  .delete(
+    verifyUser,
+    isAuthor,
+    isAuthorized("Story", "author"),
+    deleteStoryController
   );
 
 export default router;
